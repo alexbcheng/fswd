ad20ede96659b2bd09063f8a648018e49dda4a35
alex cheng <alexcheng@Alex.verizon.net>
2019-02-07 23:28:04 -0500

    20190207

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var name = "Alex";
+//alert("Hello my name is: " + name);
+name = prompt("What is your name? ");
+//alert("Hello "+ name + ", nice to meet you!");
+console.log("Hello "+ name + ", nice to meet you!");
+
+if (name.length > 2) {
+    console.log("test")
+}

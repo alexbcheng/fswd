1d248060e6ae08ee929c5a181d18fed699ae5086
alex cheng <alexcheng@Alex.verizon.net>
2019-03-12 23:37:28 -0400

    20190312

--- myJQ.js
+++ myJQ.js
@@ -5,3 +5,12 @@ $("div.highlight").css("width", "200px")
 $("#third").css("border", "2px solid orange")
 
 $("div:first-of-type").css("color", "pink")
+
+$('#add').on('click',addToList);
+
+function addToList() {
+    var item2add = $('input').val();
+    console.log(item2add);
+    $('ul').append("<li>"+item2add+"</li>");
+    $('input').val("");
+}

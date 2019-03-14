2f3c044645349428d55403571982549f80f9ec8d
alex cheng <alexcheng@Alex.verizon.net>
2019-03-14 00:25:55 -0400

    20190313

---
+++ toDoList.js
@@ -0,0 +1,22 @@
+$('ul').on('click', 'li', updateItem);
+$('ul').on('click', 'span', removeItem);
+$("input[type='text']").on('keypress', function(e) {
+    if (e.which === 13) addItem();
+});
+
+function updateItem() {
+    $(this).toggleClass("crossOut");
+}
+
+function removeItem(e) {
+    $(this).parent().fadeOut(400, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+}
+
+function addItem() {
+    var item2add = $('input').val();
+    $('ul').append("<li><span>X</span> "+item2add+"</li>");
+    $('input').val("");
+}
